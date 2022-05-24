41cf903958cc269fd3bf905a3948184ee4054911
Samuel Wakuma <SamuelWa@ethiopianairlines.com>
2022-05-24 16:53:39 +0300

    change images

--- code/galarry.php
+++ code/galarry.php
@@ -1,75 +1,73 @@
 <!DOCTYPE html>
 <html>
-<title>W3.CSS</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="bootstrap/dist/css/bootstrap.css" rel="stylesheet" type="text/css" ></link>
-<script type="text/javascript" src="bootstrap/dist/js/bootstrap.js"></script>
- <style>
-    .banner-image{
+
+<head>
+    <title>W3.CSS</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="bootstrap/dist/css/bootstrap.css" rel="stylesheet" type="text/css">
+    </link>
+    <script type="text/javascript" src="bootstrap/dist/js/bootstrap.js"></script>
+    <style>
+    .banner-image {
         height: 100vh;
     }
-</style>
-<body >
-<div  id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="image/a.jpg" class="d-block w-100 banner-image" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="image/b.jpg" class="d-block w-100 banner-image" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="image/c.jpg" class="d-block w-100 banner-image" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="image/d.jpg" class="d-block w-100 banner-image" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="image/e.jpg" class="d-block w-100 banner-image" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="image/f.jpg" class="d-block w-100 banner-image" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="image/g.jpg" class="d-block w-100 banner-image" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="image/h.jpg" class="d-block w-100 banner-image" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="image/i.jpg" class="d-block w-100 banner-image" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="image/j.jpg" class="d-block w-100 banner-image" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="image/v.jpg" class="d-block w-100 banner-image" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="image/l.jpg" class="d-block w-100 banner-image" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="image/m.jpg" class="d-block w-100 banner-image" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="image/d2.jpg" class="d-block w-100 banner-image" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="image/pp.jpg" class="d-block w-100 banner-image" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="image/p.jpg" class="d-block w-100 banner-image" alt="...">
-          </div>
+    </style>
+</head>
 
+<body>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="image/1.jpg" class="d-block w-100 banner-image" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="image/2.jpg" class="d-block w-100 banner-image" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="image/3.jpg" class="d-block w-100 banner-image" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="image/4.jpg" class="d-block w-100 banner-image" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="image/5.jpg" class="d-block w-100 banner-image" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="image/6.jpg" class="d-block w-100 banner-image" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="image/7.jpg" class="d-block w-100 banner-image" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="image/8.jpg" class="d-block w-100 banner-image" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="image/9.jpg" class="d-block w-100 banner-image" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="image/10.jpg" class="d-block w-100 banner-image" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="image/11.jpg" class="d-block w-100 banner-image" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="image/12.jpg" class="d-block w-100 banner-image" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="image/13.jpg" class="d-block w-100 banner-image" alt="...">
+            </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
         </button>
-      </div>
+    </div>
 </body>
-</html> 
+
+</html>
